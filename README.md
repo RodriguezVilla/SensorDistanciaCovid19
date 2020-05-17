706b620b527d6c60c9178e99c1830a1b3c8ff1cb
RodriguezVilla <ruben.rodriguez.villa@gmail.com>
2020-05-17 21:44:30 +0200

    v1.0.0.!

--- README.md
+++ README.md
@@ -2,16 +2,21 @@
 
 ## v1.0.0  Versión Gamma
 ### Funciones
- * Detecta obstaculos a 2m de distncia, a distancia menores de 2 metros emite pitidos
+ * Detecta obstaculos a 2m de distncia, a distancia menores de 2 metros:
+   * Inidca con pitidos.
+   * Indica con led encendido.	
 
 ![esquema Chuflusku panel horario](https://github.com/RodriguezVilla/SensorDistanciaCovid19/blob/master/SensorDistanciaCovid19_v1.X.X_esquematico.png)
 ### Hardware
  * 1 Arduino nano.
  * 1 sensor distancia HR-SR04.
+ * 1 100 Ohmios.
  * 1 modulo altavoz.
+ * 1 Resistencia 330Ohmios.
+ * 1 tira led WS82-12.
 
 ## Librerias necesarias:
- * **NewPing** by *Tim Eckel* v1.9.1 <NewPing.h>
+ * **Adafruit Neo Pixel** by *Adafruit* v1.3.4 <Adafruit_NeoPixel.h>
 
 ### Licencia ![licencia de Creative Commons Reconocimiento 4.0 Internacional](https://licensebuttons.net/l/by-nc-sa/4.0/88x31.png)
 
--- SensorDistanciaCovid19.ino
+++ SensorDistanciaCovid19.ino
@@ -1,18 +1,86 @@
-#include <NewPing.h>
+#include <Adafruit_NeoPixel.h>
 
+/////////////////////////////////////
+// Rubén R. V.
+// 17/05/2020
+// Sensor distancia Seguridad Covid 18. v1.0.0
+
+// Repositorio:
+//  - https://github.com/RodriguezVilla/SensorDistanciaCovid19
+//
+// Funciones:
+//  -Indica cuando haya un obstaculo a menos de 2m
+//
+// Hardware:
+//  * 1 Arduino nano.
+//  * 1 sensor distancia HR-SR04.
+//  * 1 100 Ohmios.
+//  * 1 modulo altavoz.
+//  * 1 Resistencia 330Ohmios.
+//  * 1 tira led WS82-12.
+//
+//////////////////////////////////////
+
+const int ledsPin = 3;
+const int numLeds = 3; 
+const int BuzzerPin = 4;
 const int EchoPin = 5;
 const int TriggerPin = 6;
-const int DistanciaMax = 200;
 
-NewPing sonar(TriggerPin,EchoPin, DistanciaMax);
+int distancia_cm=0;
+
+Adafruit_NeoPixel ledsDistSeguridad = Adafruit_NeoPixel(numLeds, ledsPin, NEO_GRB + NEO_KHZ800);
+
+uint32_t offDistSeguridad =  ledsDistSeguridad.Color(0,0,0);
+uint32_t rojoDistSeguridad =  ledsDistSeguridad.Color(130,0,0);
 
 void setup() {
   Serial.begin(9600);
+  pinMode(TriggerPin, OUTPUT);
+  pinMode(EchoPin, INPUT);
 }
 
 void loop() {
+  distancia_cm = ping(TriggerPin, EchoPin);
   delay(50);
-  Serial.print(sonar.ping_cm()); // obtener el valor en cm (0 = fuera de rango)
-  Serial.println("cm");
+  if(distancia_cm <200 && distancia_cm >0){
+    Serial.print(distancia_cm ); 
+    Serial.println("cm");
+    
+    for(int i=0; i<numLeds; i++){
+      ledsDistSeguridad.setPixelColor(i, rojoDistSeguridad);
+      ledsDistSeguridad.show();
+    }
+    
+    tone(BuzzerPin,200,1000);
 
+  }else{
+    Serial.println("Distancia de seguridad");
+    Serial.print(distancia_cm ); 
+    Serial.println("cm");
+        
+    for(int i=0; i<numLeds; i++){
+      ledsDistSeguridad.setPixelColor(i, offDistSeguridad);
+      ledsDistSeguridad.show();
+    }
+  }
+
+}
+
+//------------------------------------------------
+// Función: Vista en ->  https://www.luisllamas.es/medir-distancia-con-arduino-y-sensor-de-ultrasonidos-hc-sr04/
+//-----------------------------------------------------
+int ping(int TriggerPin, int EchoPin) {
+   long duration, distanceCm;
+   
+   digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+   delayMicroseconds(4);
+   digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+   delayMicroseconds(10);
+   digitalWrite(TriggerPin, LOW);
+   
+   duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+   
+   distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+   return distanceCm;
 }
